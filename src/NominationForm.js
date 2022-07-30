2ae711d2f2ca55d12805e4f04bb21ad34a27d6d8
Alex Kim <alex@ajpkim.com>
2022-07-30 13:08:20 -0400

    Add nomination form that connects to backend

--- src/NominationForm.js
+++ src/NominationForm.js
@@ -1,16 +1,24 @@
-function NominationForm() {
+import { useState } from "react";
 
+function NominationForm(props) {
+    const [nomination, setNomination] = useState("");
+    const handleSubmit = (e) => {
+	e.preventDefault();
+	props.sendJsonMessage({
+	    type: "nomination",
+	    message: nomination
+	})
+    }
+    const handleChange = (e) => {
+	setNomination(e.target.value);
+    }
     return (
-	<div id="nomination-form">
-	    <form>
-		<label>
-		    Nominate a movie:
-		</label>
-		<input type="text"/>
+	<div id="nomination-form-container">
+	    <form id="nomination=form" onSubmit={handleSubmit}>
+		<input placeholder="Movie Nomination" onChange={handleChange}/>
+		<input type="submit" value="Nominate"/>
 	    </form>
 	</div>
-
-
     );
 }
 
