497b12067ed7c4d730cb5de3c51cc5005d6c6787
Alex Kim <alex@ajpkim.com>
2022-08-08 00:07:16 -0400

    Initialize Room with data received from WS API instead of REST

--- src/MovieRoom.js
+++ src/MovieRoom.js
@@ -1,4 +1,4 @@
-import { useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 import useWebSocket, { ReadyState } from 'react-use-websocket'
 
 import logo from './logo.svg';
@@ -8,95 +8,75 @@ import NominationList from './NominationList.js'
 import NavBar from './NavBar.js'
 
 // Testing stuff
-const ws_path = 'ws://127.0.0.1:8000/api/test_room/'
+const ws_url = 'ws://127.0.0.1:8000/api/test_room/'
 
-function MovieRoom(props) {
-    const { readyState } = useWebSocket(ws_path, {
+export default function MovieRoom(props) {
+    const { readyState, sendJsonMessage } = useWebSocket(ws_url, {
 	onOpen: () => console.log("WS Connected!"),
 	onClose: () => console.log("WS Disconnected!"),
 	onMessage: (e) => {
 	    const data = JSON.parse(e.data);
-	    switch(data.type) {
-	    case 'nomination':
+	    switch(data.action) {
+	    case 'subscribe_to_room_nominations':
+		console.log("NEW NOMINATION");
 		addNomination(data);
 		break;
-	    case 'vote':
+	    case 'subscribe_to_room_votes':
 		addVote(data);
+		console.log("NEW VOTE");
+		break
+	    case 'retrieve':
+		setNominations(data.data.nominations);
 		break
 	    default:
 		console.error("Unknown message type");
-		console.log(e.data);
+		console.log(data);
 		break;
 	    }
-	}
+	},
+	shouldReconnect: (closeEvent) => true,
     })
 
-    const { sendJsonMessage } = useWebSocket(ws_path);
     const [nominations, setNominations] = useState([]);
 
     useEffect(() => {
 	const fetchData = async () => {
-	    const response = await fetch('http://localhost:8000/api/rooms/test_room/');
-	    const newData = await response.json();
-	    setNominations(newData.nominations);
-	    console.log(newData.nominations);
-	};
+	    sendJsonMessage({
+		action: "retrieve",
+		request_id: new Date().getTime(),
+		'name': props.room_name,
+		pk: 2
+	    })
+	}
+	const subscribe_to_room_nominations = () => {
+	    console.log("Subscribing to room nominations");
+	    sendJsonMessage({
+		action: "subscribe_to_room_nominations",
+		request_id: new Date().getTime(),
+		'room_name': props.room_name,
+	    })
+	}
+	const subscribe_to_room_votes = () => {
+	    console.log("Subscribing to room votes");
+	    sendJsonMessage({
+		action: "subscribe_to_room_votes",
+		request_id: new Date().getTime(),
+		'room_name': props.room_name,
+	    })
+	}
+
 	fetchData();
+	subscribe_to_room_nominations();
+	subscribe_to_room_votes();
+
     }, [props.room_name]);
 
-    //////////
-    // Testing the DCRF API/
-
-    const foo = () => {
-	console.log("SENDING retrieve (GET) request to server");
-	const msg = {
-	    action: "retrieve",
-	    request_id: new Date().getTime(),
-	    'room_name': props.room_name,
-	    pk: 2
-	}
-	sendJsonMessage(msg);
-    }
-
-    const subscribe_to_nomination = () => {
-	console.log("Subscirbing to nomination");
-	sendJsonMessage({
-	    action: "subscribe_to_nomination",
-	    request_id: new Date().getTime(),
-	    'room_name': props.room_name,
-	    'title': 'Moonrise Kingdom',
-	})
-    }
-
-    const subscribe_to_room = () => {
-	console.log("Subscirbing to room");
-	sendJsonMessage({
-	    action: "subscribe_to_room",
-	    request_id: new Date().getTime(),
-	    'name': props.room_name,
-	});
-    }
-    const [functionCount,setFunctionCount] = useState(0);
-
-    useEffect(() => {
-        if (functionCount === 0) { MyFunction() }
-    })
-
-    const MyFunction = () => {
-	foo();
-	subscribe_to_nomination();
-	// subscribe_to_room();
-	setFunctionCount(functionCount + 1);
-    }
-
-    //////////
 
 
     const addNomination = (data) => {
 	const nomination = {
 	    'title': data.title,
-	    'votes_yes': data.votes_yes,
-	    'votes_no': data.votes_no
+	    'votes': data.votes,
 	}
 	console.log("new nomination: " + nomination);
 	setNominations(nominations => [...nominations, nomination]);
@@ -112,13 +92,18 @@ function MovieRoom(props) {
 	    <NominationList nominations={nominations} sendJsonMessage={sendJsonMessage}/>
 	    </div>
     )
-
-
-
-
-
-
-
 }
 
-export default MovieRoom
+
+
+// const [functionCount,setFunctionCount] = useState(0);
+
+    // useEffect(() => {
+    //     if (functionCount === 0) { MyFunction() }
+    // })
+
+    // const MyFunction = () => {
+    // 	subscribe_to_room_nominations();
+    // 	subscribe_to_room_votes();
+    // 	setFunctionCount(functionCount + 1);
+    // }
